c5b92d60a9cd443ce2dcd6006c808830a4d6ae82
marchik88 <marchik88@rambler.ru>
2016-09-07 16:32:59 +0300

    ios responsive

--- mail/send.php
+++ mail/send.php
@@ -33,7 +33,7 @@ $source = $form . $user_name . $user_phone . $user_message . $user_email;
 $headers = "MIME-Version: 1.0\r\n";
 
 $headers .= "Content-type: text/html; charset=utf-8\r\n"; 
-$result = mail('marchik88@rambler.ru, Artem2431@gmail.com', "Материк - " . $template, $source, $headers);
+$result = mail('marchik88@rambler.ru, Artem2431@gmail.com', "Материк - " .$template, $source, $headers);
 
 if ($result) {
   header("Location: ../thanks.html");
